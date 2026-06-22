11b16fe9fba15cc2f10761b6de969739b185a525
theaaravgoyal <aaravkgoyalabhi1086@gmail.com>
2026-06-22 12:34:35 +0530

    image paths updated

--- src/components/AboutSection/AboutImg.jsx
+++ src/components/AboutSection/AboutImg.jsx
@@ -1,7 +1,7 @@
 function AboutImg(){
   return(
     <div className="w-full max-w-200 h-[400px] rounded-4xl overflow-hidden">
-      <img className="w-full h-full object-cover object-[center_12%]" src="public/aaravGoyal.png" alt="Aarav Goyal Portrait"/>
+      <img className="w-full h-full object-cover object-[center_12%]" src="/aaravGoyal.png" alt="Aarav Goyal Portrait"/>
     </div>
   )
 }
